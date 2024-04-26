6511423dca7a60d460a7165ff7f60718e2c6f9da
María Delgado <mariad1996@gmail.com>
2024-04-26 14:57:49 +0200

    Partial Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import useFetchData from './hooks/useFetchData.js';
 
+import Header from './components/Header.jsx';
+import Home from './components/Home.jsx'
+import FormLogin from './components/FormLogin.jsx'
+import FormRegister from './components/FormRegister.jsx'
+import Products from './components/Products.jsx'
+
+
+
 const App = () => {
     //const apiUrl = import.meta.env.VITE_APP_API_URL
 
     return (
-        <>
-        </>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/login' element={<FormLogin />} />
+                <Route path='/register' element={<FormRegister />} />
+            </Routes>
+        </Router>
     )
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
-function Header(){
+import { Link } from 'react-router-dom'
+
+function Header() {
     return (
-        <>
-        </>
+        <header>
+            <nav>
+                <div class='navMenu'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/products'>Products</Link>
+                    <Link to='/'>Mens</Link>
+                    <Link to='/'>Womens</Link>
+                    <Link to='/'>About Us</Link>
+                </div>
+                <input class='searchBar' type='text' />
+                <div class='interactive'>
+                    <Link to='/login'>Sign In</Link>
+                    <Link to='/register'>Sign Up</Link>
+                    <Link to='/logout'>Log Out</Link>
+                    <a href=''><img class='cart' src='src/assets/icons/cart.png' /></a>
+                </div>
+            </nav>
+        </header>
     )
 }
 
