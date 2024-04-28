0b6b711f467d50febbbae11f3d35e3d20bbcdde9
jose-sinacento <ruizherrera.jose@gmail.com>
2024-04-28 19:59:03 +0200

    home carrusel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,28 +10,58 @@ function Home() {
         const imgNode = listNode.querySelectorAll("li>img")[currentIndex];
         if (imgNode) {
             imgNode.scrollIntoView({
-                behavior: "smooth"
+                behavior:"smooth"
             });
         }
 
     }, [currentIndex])
+    const scrollToImage = (direction) => {
+        if(direction == 'prev') {
+            setCurrentIndex(curr => {
+                const isFirstSlide = currentIndex === 0;
+                return isFirstSlide ? 0 : curr -1;
+            })
+        } else {
+            const isLastSlide = currentIndex === data.length - 1;
+            if(!isLastSlide) {
+                setCurrentIndex(curr => curr +1);
+            }
+        }
+    }
 
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+
+    }
 
     return (
         <>
             <div className="carrusel-container">
                 <div className="slider-container">
+                    <div className='leftArrow' onClick={() =>scrollToImage('prev')}>&#10092;</div>
+                    <div className='rightArrow' onClick={() =>scrollToImage('next')}>&#10093;</div>
                     <div className="container-images">
                         <h1>SOY UN CARRUSEL</h1>
-                        <ul ref={listRef}>
-                            {carruselData.map(item => (
-                                <li key={item.id}>
-                                    <img src={item.imgUrl} width="100px" height="100px" alt="" />
+                        <ul ref={listRef} >
+                            {
+                            carruselData.map((item) => {
+                                return <li key={item.id}>
+                                    <img src={item.imgUrl} width={1800}height={500} alt="" />
                                 </li>
-                            ))
+                                })
                             }
                         </ul>
                     </div>
+                </div>
+                <div className='dots-container'>
+                    {
+                        carruselData.map((_, idx) => (
+                            <div key={idx} className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
+                            onClick={()=> goToSlide(idx)}>
+                                &#9865;
+                            </div>
+                        ))
+                    }
                 </div>
                 <section>
                     <div className="banner-1">
