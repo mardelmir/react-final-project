9006bf1a7d24578ad176874819cc13f7e064e229
María Delgado <mariad1996@gmail.com>
2024-04-28 18:55:38 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,9 @@
 import React, { useRef, useState, useEffect } from 'react'
-//import { carruselData } from '../assets/carruselData.js';
+import { carruselData } from '../assets/carruselData.js';
 
 function Home() {
     const listRef = useRef();
     const [currentIndex, setCurrentIndex] = useState(0);
-
-    // Un moment que quiero ver si funciona
-    // Otra vez se ha roto :(
-    const carruselData = [
-        { id: 1, imgUrl: "/src/assets/carrusel-images/323400_9630_XL.jpeg" },
-        { id: 2, imgUrl: "/src/assets/carrusel-images/barfussschuhe_titel1_1024x1024.jpeg" },
-        { id: 3, imgUrl: "/src/assets/carrusel-images/michal-parzuchowski-zoJgYWHOYaw-unsplash.jpeg" },
-        { id: 4, imgUrl: "/src/assets/carrusel-images/Try-On-Event-Vote-Cover-Photo-Anya-Samantha-768x432.jpeg" },
-        { id: 5, imgUrl: "/src/assets/carrusel-images/anthony-tori-z3uRzmu22nE-unsplash.jpeg" }
-    ]
-
 
     useEffect(() => {
         const listNode = listRef.current;
@@ -35,14 +24,11 @@ function Home() {
                     <div className="container-images">
                         <h1>SOY UN CARRUSEL</h1>
                         <ul ref={listRef}>
-                            {
-                                carruselData.map((item) => {
-                                    return (
-                                        <li key={item.id}>
-                                            <img src={item.imgUrl} width="100px" height="100px" alt="" />
-                                        </li>
-                                    )
-                                })
+                            {carruselData.map(item => (
+                                <li key={item.id}>
+                                    <img src={item.imgUrl} width="100px" height="100px" alt="" />
+                                </li>
+                            ))
                             }
                         </ul>
                     </div>
