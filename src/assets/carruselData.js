cb85e258afb338cb0596e5246b8275ee5cc405a6
María Delgado <mariad1996@gmail.com>
2024-04-29 15:01:42 +0200

    css carrusel

--- src/assets/carruselData.js
+++ src/assets/carruselData.js
@@ -1,8 +1,9 @@
 export const carruselData = [
-    // { id: 1, imgUrl: "../assets/carrusel-images/323400_9630_XL.jpeg" },
-    { id: 1, imgUrl: "/src/assets/carrusel-images/323400_9630_XL.jpeg" },
-    { id: 2, imgUrl: "/src/assets/carrusel-images/barfussschuhe_titel1_1024x1024.jpeg" },
-    { id: 3, imgUrl: "/src/assets/carrusel-images/michal-parzuchowski-zoJgYWHOYaw-unsplash.jpeg" },
-    { id: 4, imgUrl: "/src/assets/carrusel-images/Try-On-Event-Vote-Cover-Photo-Anya-Samantha-768x432.jpeg" },
-    { id: 5, imgUrl: "/src/assets/carrusel-images/anthony-tori-z3uRzmu22nE-unsplash.jpeg" }
+    { id: 1, imgUrl: "/src/assets/carrusel-images/cart.png" },
+    // { id: 2, imgUrl: "/src/assets/carrusel-images/ice.jpg" },
+    // { id: 1, imgUrl: "/src/assets/carrusel-images/323400_9630_XL.jpeg" },
+    // { id: 2, imgUrl: "/src/assets/carrusel-images/barfussschuhe_titel1_1024x1024.jpeg" },
+    // { id: 3, imgUrl: "/src/assets/carrusel-images/michal-parzuchowski-zoJgYWHOYaw-unsplash.jpeg" },
+    // { id: 4, imgUrl: "/src/assets/carrusel-images/Try-On-Event-Vote-Cover-Photo-Anya-Samantha-768x432.jpeg" },
+    // { id: 5, imgUrl: "/src/assets/carrusel-images/anthony-tori-z3uRzmu22nE-unsplash.jpeg" }
 ]
