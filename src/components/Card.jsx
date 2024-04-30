221cfe91f8d6b47b506eb098f204c40ccf0b9200
María Delgado <mariad1996@gmail.com>
2024-04-30 13:05:38 +0200

    Card CSS responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,13 +16,17 @@ function Card() {
                 : (
                     <div className='card-container'>
                         <div key={product._id} className='card'>
-                            <h2>{product.name}</h2>
-                            <img src={product.img} alt={product.name} />
-                            <p><span>Gender</span>: {product.category.gender}</p>
-                            <p><span>Use</span>: {product.category.use}</p>
-                            {/* <p><span>Size</span>: {product.size}</p> */}
-                            <p><span>Price</span>: {product.price}€</p>
-                            <Link to='/products'><button>Go back</button></Link>
+                            <div className='card-img'>
+                                <img src={product.img} alt={product.name} />
+                            </div>
+                            <div className='detail'>
+                                <h2>{product.name}</h2>
+                                <p><span>Gender</span>: {product.category.gender}</p>
+                                <p><span>Use</span>: {product.category.use}</p>
+                                {/* <p><span>Size</span>: {product.size}</p> */}
+                                <p><span>Price</span>: {product.price}€</p>
+                                <Link to='/products'><button>Go back</button></Link>
+                            </div>
                         </div>
                     </div>
                 )
