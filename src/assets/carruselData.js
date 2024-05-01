539335c08b46e7785dcda2a2de5b4b59794a54a5
María Delgado <mariad1996@gmail.com>
2024-05-01 17:21:24 +0200

    css modifications

--- src/assets/carruselData.js
+++ src/assets/carruselData.js
@@ -1,6 +1,6 @@
 export const carruselData = [
     { id: 1, imgUrl: "/images/sky.jpg" },
-    { id: 2, imgUrl: "/images/crystal.jpg" }
+    { id: 2, imgUrl: "/images/star.jpg" }
     // { id: 1, imgUrl: "/src/assets/carrusel-images/323400_9630_XL.jpeg" },
     // { id: 2, imgUrl: "/src/assets/carrusel-images/barfussschuhe_titel1_1024x1024.jpeg" },
     // { id: 3, imgUrl: "/src/assets/carrusel-images/michal-parzuchowski-zoJgYWHOYaw-unsplash.jpeg" },
--- src/components/NewProduct.jsx
+++ src/components/NewProduct.jsx
@@ -1,30 +1,40 @@
 import '../styles/Form.css'
-import { useState, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 function NewProduct() {
-    const [] = useState('')
+    const [selectedFile, setSelectedFile] = useState(null);
+    const formRef = useRef()
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
+    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const urlPost = 'http://localhost:8080/api/v1/admin/'
-        const payload = { email, password, role }
+        const formData = new FormData(formRef.current)
+        const values = Object.fromEntries(formData);
+        console.log(values)
+        // const urlPost = 'http://localhost:8080/api/v1/admin/'
+        // const payload = { email, password, role }
 
-        try {
-            await fetch(urlPost, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(payload)
-            })
-            navigate('/')
-        }
-        catch (error) { console.log(error) }
+        // try {
+        //     await fetch(urlPost, {
+        //         method: 'POST',
+        //         headers: { 'Content-Type': 'application/json' },
+        //         body: JSON.stringify(payload)
+        //     })
+        //     navigate('/')
+        // }
+        // catch (error) { console.log(error) }
     }
 
     return (
         <div className='form-container'>
             <h2>Add new product</h2>
-            <form className='form' onSubmit={handleSubmit}>
+            <form className='form' onSubmit={handleSubmit} ref={formRef}>
                 <label htmlFor='name'>Name</label>
                 <input
                     type='text'
@@ -44,42 +54,53 @@ function NewProduct() {
                     required />
 
                 <label htmlFor='img'>Image</label>
+                <div className="custom-file-upload" onClick={() => img.click()}>
+                    {selectedFile ? selectedFile.name : 'Select file'}
+                </div>
                 <input
                     type='file'
                     id='img'
                     name='img'
-                    accept='image/*' />
+                    accept='image/*'
+                    style={{ display: 'none' }}
+                    onChange={handleFileChange} />
 
-                <label htmlFor='category'>Category</label>
+                <label>Category</label>
                 <div className='option-container'>
                     <select name='gender'>
+                        <option value='' hidden>Gender</option>
                         <option value='Man' name='Man'>Man</option>
                         <option value='Woman' name='Woman'>Woman</option>
                     </select>
                     <select name='use'>
+                        <option value='' hidden>Use</option>
                         <option value='Lifestyle' name='Lifestyle'>Lifestyle</option>
                         <option value='Performance' name='Performance'>Performance</option>
                     </select>
                 </div>
 
-                <label htmlFor='size'>Size</label>
                 <div className='option-container'>
-                    <input
-                        type='number'
-                        id='size'
-                        name='size'
-                        min='35'
-                        max='49'
-                        step='1'
-                        placeholder='Size'
-                        required />
-                    <input
-                        type='number'
-                        id='cuantity'
-                        name='cuantity'
-                        step='1'
-                        placeholder='Cuantity'
-                        required />
+                    <div className='option'>
+                        <label htmlFor='size'>Size</label>
+                        <input
+                            type='number'
+                            id='size'
+                            name='size'
+                            min='35'
+                            max='49'
+                            step='1'
+                            required />
+                    </div>
+                    <div className='option'>
+                        <label htmlFor='cuantity'>Cuantity</label>
+                        <input
+                            type='number'
+                            id='cuantity'
+                            name='cuantity'
+                            step='1'
+                            required />
+                    </div>
+
                 </div>
                 <div className='btn-container'>
                     <button className='formBtn' type='submit'>Create</button>
